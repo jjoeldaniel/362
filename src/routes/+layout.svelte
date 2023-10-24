60f77acfaaef879e8d17a1d0f703793fc3895659
Joel Daniel Rico <joeldanielrico@csu.fullerton.edu>
2023-10-23 22:04:53 -0700

    add homepage text

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -1,5 +1,11 @@
 <script>
-  import "../app.css";
+	import '../app.css';
 </script>
 
-<slot />
+<div class="flex h-screen">
+	<div class="m-auto max-w-lg">
+		<h1 class="pb-4 text-4xl font-semibold">Insert Cool Name Here</h1>
+		<blockquote class="pb-8 text-xl font-medium italic">What a cool project.</blockquote>
+		<slot />
+	</div>
+</div>
--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,2 +1,15 @@
-<h1>Welcome to SvelteKit</h1>
-<p>Visit <a href="https://kit.svelte.dev">kit.svelte.dev</a> to read the documentation</p>
+<div class="space-y-3">
+	<p>
+		Our website is designed to help college students plan their remaining courses by uploading their
+		college audit PDF.
+	</p>
+	<p>
+		With our user-friendly interface, students can easily upload their audit and view their
+		completed courses, remaining requirements, and recommended courses to take. Students can save their progress and come back to it later, making it easy to plan their
+		courses over multiple semesters.
+	</p>
+	<p>
+		Whether you're a freshman just starting out or a senior nearing graduation, let us help you stay
+		on track and plan your courses with confidence.
+	</p>
+</div>
