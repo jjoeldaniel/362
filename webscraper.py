ceccaf616dbcaf8df7cbb18a51726cc48f989159
AngelF9 <112910128+AngelF9@users.noreply.github.com>
2023-10-24 19:51:42 -0700

    testscraper

--- webscraper.py
+++ webscraper.py
@@ -1,23 +1,43 @@
-import requests
-from bs4 import BeautifulSoup
+from selenium import webdriver
+from selenium.webdriver.common.by import By
+import time
 
-url = 'https://catalog.fullerton.edu/preview_program.php?catoid=70&poid=32659'  # Replace with the URL you want to test.
+# load the page
+URL = "https://catalog.fullerton.edu/preview_program.php?catoid=70&poid=32659"
+driver = webdriver.Chrome()
+driver.get(URL)
+time.sleep(5)  # wait for the page to load
 
-# Test the connection using requests
-response = requests.get(url)
+# locate all course links
+course_links = driver.find_elements(By.CSS_SELECTOR, 'li.acalog-course span a')
 
-if response.status_code == 200:
-    print("Connection successful!")
-    # Parse the HTML content with Beautiful Soup
-    soup = BeautifulSoup(response.text, 'html.parser')
+unique_elements = []  # list to store unique texts of elements
 
-    # Now you can work with the parsed HTML content
-    # For example, you can find a specific element by its tag or class:
-    element = soup.find('div', class_='acalog-core')
-    # print(element.text)
-    courses = element.find_all('li',class_='acalog-course')
-    print(courses[0])
-    # for i in range(len(element.text)):
-    #     print(element.text) 
-else:
-    print("Connection failed. Status code:", response.status_code)
+# iterate through each course link
+for index, link in enumerate(course_links):
+    link.click()
+    time.sleep(2)
+    
+    xpath_selector_all = ("//*[@id='gateway-page']/body/table/tbody/tr[3]/td[2]/"
+                          "table/tbody/tr[2]/td[2]/table/tbody/tr/td/table/tbody/"
+                          "tr[2]/td/div/div/div/div/div/ul/li/span/"
+                          "table/tbody/tr/td/div")
+    
+    elements = driver.find_elements(By.XPATH, xpath_selector_all)
+    
+for element in elements:
+        element_text = element.text
+        if element_text not in unique_elements:  # check if the text is not a duplicate
+            unique_elements.append(element_text)  # add each element's text to the list
+
+driver.quit()
+
+# Write all unique elements to a text file
+with open('elements.txt', 'w', encoding='utf-8') as file:
+    for text in unique_elements:
+        file.write(text + '\n')
+        file.write("------------------------------------------------\n")
+
+"""
+this scrapes only lower and upper and sends to a text file
+"""
