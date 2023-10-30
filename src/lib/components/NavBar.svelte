16e699ca835d468b6fd9f98f3575b1cd6c3d59e0
Joel Daniel Rico <joeldanielrico@csu.fullerton.edu>
2023-10-30 01:15:29 -0700

    implement navbar

---
+++ src/lib/components/NavBar.svelte
@@ -0,0 +1,84 @@
+<script>
+	let isMenuOpen = false;
+
+	function toggleMenu() {
+		isMenuOpen = !isMenuOpen;
+	}
+
+	import Constants from '$lib/constants.js';
+</script>
+
+<nav class="flex items-center justify-between flex-wrap bg-gray-800 p-6">
+	<div class="flex items-center flex-shrink-0 text-white mr-6">
+		<a href="/"><span class="font-semibold text-xl tracking-tight">{Constants.APP_NAME}</span></a>
+	</div>
+	<div class="block md:hidden">
+		<button
+			class="flex items-center px-3 py-2 border rounded text-gray-200 border-gray-400 hover:text-white hover:border-white"
+			on:click={toggleMenu}
+		>
+			<svg class="fill-current h-3 w-3" viewBox="0 0 20 20" xmlns="http://www.w3.org/2000/svg">
+				<title>Menu</title>
+				<path d="M0 3h20v2H0V3zm0 6h20v2H0V9zm0 6h20v2H0v-2z" />
+			</svg>
+		</button>
+	</div>
+
+	{#if isMenuOpen}
+		<div class="w-full flex flex-row md:hidden flex-grow">
+			<div class="text-lg">
+				<a href="#responsive-header" class="block mt-4 text-gray-200 hover:text-white mr-4">
+					About
+				</a>
+				<a href="#responsive-header" class="block mt-4 text-gray-200 hover:text-white mr-4">
+					Tutorial
+				</a>
+			</div>
+			<div class="flex flex-col ml-auto">
+				<a
+					href="#"
+					class="inline-block text-sm px-4 py-2 leading-none border rounded text-white border-white hover:border-transparent hover:text-gray-800 hover:bg-white mt-4"
+				>
+					Register
+				</a>
+				<a
+					href="#"
+					class="inline-block text-sm px-4 py-2 leading-none border rounded text-white border-white hover:border-transparent hover:text-gray-800 hover:bg-white mt-4"
+				>
+					Sign In
+				</a>
+			</div>
+		</div>
+	{/if}
+
+	<div class="w-full hidden md:flex flex-grow md:items-center md:w-auto">
+		<div class="text-sm md:flex-grow">
+			<a
+				href="#responsive-header"
+				class="block mt-4 md:inline-block md:mt-0 text-gray-200 hover:text-white mr-4"
+			>
+				About
+			</a>
+			<a
+				href="#responsive-header"
+				class="block mt-4 md:inline-block md:mt-0 text-gray-200 hover:text-white mr-4"
+			>
+				Tutorial
+			</a>
+		</div>
+		<div class="space-x-5">
+			<a
+				href="#"
+				class="inline-block text-sm px-4 py-2 leading-none border rounded text-white border-white hover:border-transparent hover:text-gray-800 hover:bg-white mt-4 md:mt-0"
+			>
+				Register
+			</a>
+			<a
+				href="#"
+				class="inline-block text-sm px-4 py-2 leading-none border rounded text-white border-white hover:border-transparent hover:text-gray-800 hover:bg-white mt-4 md:mt-0"
+			>
+				Sign In
+			</a>
+		</div>
+	</div>
+</nav>
--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -1,11 +1,16 @@
 <script>
 	import '../app.css';
+	import NavBar from '$lib/components/NavBar.svelte';
+	import Constants from '$lib/constants.js';
 </script>
 
-<div class="flex h-screen">
-	<div class="m-auto max-w-lg">
-		<h1 class="pb-4 text-4xl font-semibold">Insert Cool Name Here</h1>
-		<blockquote class="pb-8 text-xl font-medium italic">What a cool project.</blockquote>
-		<slot />
+<div class="h-screen">
+	<NavBar />
+	<div class="flex mt-24">
+		<div class="m-auto max-w-lg">
+			<h1 class="pb-4 text-4xl font-semibold">{Constants.APP_NAME}</h1>
+			<blockquote class="pb-8 text-xl font-medium italic">What a cool project.</blockquote>
+			<slot />
+		</div>
 	</div>
 </div>
