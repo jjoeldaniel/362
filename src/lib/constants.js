1a0543e2eb1dc3c34da587c9385f923f2f2074c7
jjoeldaniel <joeldanielrico@csu.fullerton.edu>
2023-12-02 17:31:12 -0800

    update app name

--- src/lib/constants.js
+++ src/lib/constants.js
@@ -1,5 +1,6 @@
 // Constants class for storing app-wide constants
 export default class Constants {
-    static APP_NAME = "🚀 Cool App Name";
-    static APP_DESCRIPTION = "Elevate your college planning experience with our user-friendly app! Seamlessly upload your college audit PDF to visualize completed courses, track remaining requirements, and discover recommended courses. Our intuitive interface empowers students to plan across semesters, allowing you to save progress and confidently navigate your academic journey. Perfect for freshmen and seniors alike, let us be your guide to stress-free course planning!";
+	static APP_NAME = '🚀 Class Quest';
+	static APP_DESCRIPTION =
+		'Elevate your college planning experience with our user-friendly app! Seamlessly upload your college audit PDF to visualize completed courses, track remaining requirements, and discover recommended courses. Our intuitive interface empowers students to plan across semesters, allowing you to save progress and confidently navigate your academic journey. Perfect for freshmen and seniors alike, let us be your guide to stress-free course planning!';
 }
