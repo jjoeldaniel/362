429fce25aa95744f4ddc913183cf94d8e655d878
jjoeldaniel <joeldanielrico@csu.fullerton.edu>
2023-12-02 17:48:54 -0800

    add /courses

---
+++ src/routes/courses/+page.svelte
@@ -0,0 +1,11 @@
+<script lang="ts">
+	export let data;
+
+	let { session, supabase, profile } = data;
+	$: ({ session, supabase, profile } = data);
+
+	let username: string = profile?.username ?? '';
+	let avatarUrl: string = profile?.avatar_url ?? '';
+</script>
+
+<p>hi {username}</p>
