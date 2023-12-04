7b9c45213c32a6ae81dd8ae51d84ccbf5bbaa847
jjoeldaniel <joeldanielrico@csu.fullerton.edu>
2023-12-03 23:26:42 -0800

    improve root ui

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -2,22 +2,30 @@
 	import Constants from '$lib/constants.js';
 </script>
 
-<h1 class="pb-4 text-4xl font-semibold">{Constants.APP_NAME}</h1>
-<blockquote class="pb-8 text-xl font-medium italic">What a cool project.</blockquote>
+<div class="flex flex-col items-center justify-center">
+	<div
+		class="flex flex-col items-center justify-center bg-gradient-to-b md:px-10 pt-10 mb-5 md:mb-12 text-white from-indigo-900 via-indigo-700 to-indigo-500"
+	>
+		<h1 class="pb-4 text-4xl md:text-5xl font-extrabold tracking-wider">{Constants.APP_NAME}</h1>
+		<blockquote class="pb-8 text-xl font-medium italic max-w-2xl text-center">
+			"Embark on a journey of knowledge and discovery with our visionary project."
+		</blockquote>
+	</div>
 
-<div class="space-y-3">
-	<p>
-		Our website is designed to help college students plan their remaining courses by uploading their
-		college audit PDF.
-	</p>
-	<p>
-		With our user-friendly interface, students can easily upload their audit and view their
-		completed courses, remaining requirements, and recommended courses to take. Students can save
-		their progress and come back to it later, making it easy to plan their courses over multiple
-		semesters.
-	</p>
-	<p>
-		Whether you're a freshman just starting out or a senior nearing graduation, let us help you stay
-		on track and plan your courses with confidence.
-	</p>
+	<div class="space-y-5 max-w-3xl text-lg">
+		<p>
+			Elevate your academic experience with our cutting-edge platform, meticulously crafted to
+			empower college students in planning their educational trajectory.
+		</p>
+		<p>
+			Seamlessly upload your college audit PDF through our intuitive interface, unveiling a
+			comprehensive overview of completed courses, outstanding requirements, and personalized
+			recommendations.
+		</p>
+		<p>
+			Effortlessly save your progress, allowing you to curate your academic path with ease over
+			multiple semesters. Whether you're a fledgling freshman or a seasoned senior, entrust us to
+			guide you towards success with confidence and clarity.
+		</p>
+	</div>
 </div>
