a6820e69bec46267a72d9a6b9268e39279f8504e
jjoeldaniel <joeldanielrico@csu.fullerton.edu>
2023-12-03 22:46:14 -0800

    Add course completion progress bar

--- src/lib/constants.js
+++ src/lib/constants.js
@@ -3,4 +3,11 @@ export default class Constants {
 	static APP_NAME = '🚀 Class Quest';
 	static APP_DESCRIPTION =
 		'Elevate your college planning experience with our user-friendly app! Seamlessly upload your college audit PDF to visualize completed courses, track remaining requirements, and discover recommended courses. Our intuitive interface empowers students to plan across semesters, allowing you to save progress and confidently navigate your academic journey. Perfect for freshmen and seniors alike, let us be your guide to stress-free course planning!';
+	static ELECTIVES_COUNT = 32;
+	static UPPER_DIV_COUNT = 11;
+	static GRAD_REQS_COUNT = 2;
+	static SCI_MATH_COUNT = 20;
+	static LOWER_DIV_COUNT = 8;
+	static MATH_COUNT = 5;
+	static TOTAL_COUNT = 78;
 }
--- src/routes/courses/+page.svelte
+++ src/routes/courses/+page.svelte
@@ -1,6 +1,7 @@
 <script lang="ts">
 	import Avatar from '$lib/components/Avatar.svelte';
 	import CourseList from '$lib/components/CourseList.svelte';
+	import Constants from '$lib/constants.js';
 
 	export let data;
 
@@ -13,6 +14,7 @@
 	const divisions = {
 		lower_div: 'Lower Division',
 		sci_math: 'Science and Math',
+		math: 'Math',
 		electives: 'Electives',
 		upper_div: 'Upper Division',
 		grad_reqs: 'Graduation Requirements'
@@ -20,6 +22,12 @@
 
 	// TODO: Load completions from Supabase
 	let completions: any = profile?.completions ?? '{}';
+
+	let progress: number = 0;
+	$: {
+		const completed = Object.values(completions).filter((c) => c).length;
+		progress = (completed / Constants.TOTAL_COUNT) * 100;
+	}
 </script>
 
 <h1 class="text-3xl font-bold mb-4">Courses</h1>
@@ -27,8 +35,12 @@
 <div class="bg-gray-800 p-6 rounded-lg flex items-center space-x-4">
 	<Avatar {data} bind:url={avatarUrl} size={150} />
 
-	<div>
+	<div class="flex-1">
 		<h2 class="text-3xl md:text-4xl text-center pl-5 font-bold text-white">Welcome, {username}!</h2>
+
+		<div class="mt-6 w-full bg-gray-600 h-4 rounded-full overflow-hidden">
+			<div class="bg-blue-500 h-full" style="width: {progress}%" />
+		</div>
 	</div>
 </div>
 
