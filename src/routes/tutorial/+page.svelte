a9d456d82d7359f1e1f1dbda1ba90adc5ea47de5
Joel Daniel Rico <joeldanielrico@csu.fullerton.edu>
2023-12-05 20:08:53 -0800

    format and add missing bottom gradient bar

--- src/routes/tutorial/+page.svelte
+++ src/routes/tutorial/+page.svelte
@@ -1,26 +1,32 @@
 <script>
-    import Constants from '$lib/constants.js';
-    const Title = '🚀 Mission: Tutorial 🌌'
+	import Constants from '$lib/constants.js';
+	const Title = '🚀 Mission: Tutorial 🌌';
 </script>
 
 <div class="flex flex-col items-center justify-center">
-    <div
-        class="rounded-3xl rounded-b-none flex flex-col items-center justify-center bg-gradient-to-b md:px-10 pt-10 mb-5 md:mb-12 text-white from-indigo-900 via-indigo-700 to-indigo-500"
-    >
-        <h1 class="pb-4 text-3xl md:text-4xl font-extrabold tracking-wider">{Title}</h1>
-        <blockquote class="pb-8 px-2 md:text-xl text-lg font-medium italic max-w-2xl text-center">
-            "The most beautiful thing we can experience is the mysterious. It is the source of all true art and science."
+	<div
+		class="rounded-3xl rounded-b-none flex flex-col items-center justify-center bg-gradient-to-b md:px-10 pt-10 mb-5 md:mb-12 text-white from-indigo-900 via-indigo-700 to-indigo-500"
+	>
+		<h1 class="pb-4 text-3xl md:text-4xl font-extrabold tracking-wider">{Title}</h1>
+		<blockquote class="pb-8 px-2 md:text-xl text-lg font-medium italic max-w-2xl text-center">
+			"The most beautiful thing we can experience is the mysterious. It is the source of all true
+			art and science."
 			<br />
 			<span class="font-bold">- Albert Einstein</span>
-        </blockquote>
-    </div>
+		</blockquote>
+	</div>
 
-    <div class="space-y-5 max-w-3xl text-lg">
-        <p>
-            <strong>🚀 Launch Phase:</strong> Strap in and prepare for lift-off! Click 'Login' on the top right to activate your mission control center.
-        </p>
-        <p>
-            <strong>🌌 Exploration Phase:</strong> Set your trajectory! Head over to your 'Courses' tab. This is your star map, guiding you through classes and showing what galaxies remain unexplored. 
-        </p>
-    </div>
+	<div class="space-y-5 max-w-3xl text-lg">
+		<p>
+			<strong>🚀 Launch Phase:</strong> Strap in and prepare for lift-off! Click 'Login' on the top right
+			to activate your mission control center.
+		</p>
+		<p>
+			<strong>🌌 Exploration Phase:</strong> Set your trajectory! Head over to your 'Courses' tab. This
+			is your star map, guiding you through classes and showing what galaxies remain unexplored.
+		</p>
+	</div>
+
+	<div class="mt-10 w-full h-8 bg-gradient-to-r from-pink-500 via-purple-500 to-indigo-500" />
 </div>
+
